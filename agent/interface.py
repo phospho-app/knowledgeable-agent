6061305983dff76f1aa7fb4588e0a593e1752f48
Pierre-LouisBJT <plbiojout@gmail.com>
2023-05-13 16:54:20 +0200

    Updated agent template

--- agent/interface.py
+++ agent/interface.py
@@ -3,8 +3,14 @@ This will be the file handling the interfacing between the dev user agent repo a
 """
 
 # This is the function that will be called when a question is asked to the agent
-def ask(query): 
+def ask(messages): 
     # Your code here
-    response = 'The question was : {}'.format(query) # This is just a dummy response
+
+    # This is an example of how to get the last message received by the agent
+    if len(messages) == 0:
+        response = "No message received"
+    else:
+        last_message = messages[-1]
+        response = "Your question was : " + last_message["content"]
 
     return response #str
